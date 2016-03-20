5e55206a0b0580d0a28a4a9c5e96a74a3b1b017c
Prasenjit Sharan <prasenjit@jova.io>
2016-03-21 01:02:33 +0530

    Fetch one chapter, works

--- app.js
+++ app.js
@@ -24,13 +24,37 @@ function get(url) {
 }
 
 function getJSON(url) {
-  return get(url).then(JSON.parse);
+  return get(url).then(JSON.parse).catch(function(err) {
+    console.log("getJSON failed for", url, err);
+    throw err;
+  });
 }
 
-// Use the get funcion defined above
-// get returns a promise, thus it's thenable
-getJSON('story.json').then(function(response) {
-  console.log('JSON: ' + response);
-}, function(error) {
-  console.error('Failure! ' + error);
+var storyPromise;
+
+function getChapter(i) {
+  storyPromise = storyPromise || getJSON('story.json');
+
+  return storyPromise.then(function(story) {
+    return getJSON(story.chapterUrls[i]);
+  });
+}
+
+// getChapter(0).then(function(chapter) {
+//   console.log('Chapter 1: ' + chapter);
+//   return getChapter(1);
+// }).then(function(chapter) {
+//   console.log('Chapter 2: ' + chapter);
+// }, function(error) {
+//   console.error('Failure! ' + error);
+// });
+
+getJSON('story.json').then(function(story) {
+  return getJSON(story.chapterUrls[0]);
+}).then(function(chapter1) {
+  addHtmlToPage(chapter1.html);
+}).catch(function() {
+  addTextToPage("Failed to show chapter");
+}).then(function() {
+  document.querySelector('.spinner').style.display = 'none';
 });
